67e4f200ba497c9842f909362ea8f54ef5746f36
SamZ1207 <82178381+SamZ1207@users.noreply.github.com>
2021-04-20 15:16:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+function myFunction() {  var navBar = document.getElementById("myTopnav");  if (navBar.className === "topnav") {	navBar.className += " responsive";  } else {	navBar.className = "topnav";  }}
